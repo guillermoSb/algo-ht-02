fd1642e4c6f65adac9db9ffdf1f0aae116f42f58
Guillermo <guillermo@inlandlogistics.co>
2022-01-31 16:54:08 -0600

    Implementation is now able to calculate

--- src/com/calculadora/Main.java
+++ src/com/calculadora/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("Calculator");
         PostfixCalculator calculator = new PostfixCalculator();
         // TODO: Usar un menu e ingresar los contenidos del archivo de texto en el metodo calculate.
-        System.out.println(calculator.calculate("1 2 + 4 * 3 +"));
+        System.out.println(calculator.calculate("6 2 3 + *"));
     }
 }
--- src/com/calculadora/PostfixCalculator.java
+++ src/com/calculadora/PostfixCalculator.java
@@ -1,27 +1,28 @@
 package com.calculadora;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
-public class PostfixCalculator implements Stack<String>, Calculator{
+public class PostfixCalculator implements Stack<Double>, Calculator{
 
-    Vector<String> data;
+    Vector<Double> data;
 
     PostfixCalculator() {
-        data = new Vector<String>();
+        data = new Vector<Double>();
     }
 
     @Override
-    public void add(String item) {
+    public void add(Double item) {
         data.add(item);
     }
 
     @Override
-    public String remove() {
+    public Double remove() {
         return data.remove(size() - 1);
     }
 
     @Override
-    public String peek() {
+    public Double peek() {
         return data.get(size() - 1);
     }
 
@@ -37,9 +38,60 @@ public class PostfixCalculator implements Stack<String>, Calculator{
 
     @Override
     public double calculate(String operation) {
-        do {
+        // Split the operation into sub components
+        String[] splittedOperation = operation.split(" ");
+        // Itterate through each value on the operation
+        for (String value:
+             splittedOperation) {
+            // TODO: Handle error
+            if (!isNumber(value) && !isOperator(value)) {
+                return 0;
+            }
 
-        } while (data.size() > 0);
-        return 0;
+            // If the value is a number, add it to the stack
+            if (isNumber(value)) {
+               add(Double.parseDouble(value));
+            }
+            // If the value is an operator, start operating two values
+            if(isOperator(value)) {
+                Double operatorA = remove();
+                Double operatorB = remove();
+                String operator = value;
+                Double result = operate(operatorA, operatorB, operator);
+                add(result);
+            }
+        }
+        return remove();
+    }
+
+    /**
+     * Operate two values
+     * @param a Operator 1
+     * @param b Operator 2
+     * @param operator operation. +, -, /, *
+     * @return
+     */
+    private double operate(Double a, Double b, String operator) {
+        if (operator.equals("+")) return a + b;
+        if (operator.equals("-")) return a + b;
+        if (operator.equals("*")) return a * b;
+        if (operator.equals("/")) return a / b;
+        return 0;   // Last instance
+    }
+
+    private boolean isNumber(String value) {
+        try {
+            Double number = Double.parseDouble(value);
+            return true;
+        } catch (Exception e) {
+            return  false;
+        }
+    }
+
+    private boolean isOperator(String value) {
+        if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")) {
+            return true;
+        }
+        return false;
     }
 }
