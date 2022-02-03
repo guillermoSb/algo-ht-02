f0c9c66c828a05e2f26e7485a399871c35236f63
Guillermo <guillermo@inlandlogistics.co>
2022-02-02 18:52:52 -0600

    Fixed implementation

--- src/com/calculadora/Main.java
+++ src/com/calculadora/Main.java
@@ -8,35 +8,31 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Calculator");
-        PostfixCalculator calculator = new PostfixCalculator();
+        PostfixCalculator calculator = new PostfixCalculator(); //
         Scanner sc = new Scanner(System.in);
         File file = new File("datos.txt");
         Scanner rd;
         try {
             rd = new Scanner(file);
+            Vista v = new Vista();
+            v.saludo();
+            int ot = 0;
+            while (ot != 2) {
+                v.menu();
+                ot = sc.nextInt();
+                if (ot == 1) {
+                    while (rd.hasNextLine()){
+                        System.out.println(calculator.calculate(rd.nextLine()));
+                    }
+
+
+                }
+            }
         } catch (FileNotFoundException e) {
+            System.out.println("NO");
             e.printStackTrace();
         }
-        Vista v = new Vista();
-        v.saludo();
-        int ot = 0;
-        while (ot != 3) {
-            v.menu();
-            ot = sc.nextInt();
-            if (ot == 1) {
-                v.ask();
-                String op = sc.nextLine();
-                v.respuesta();
-                System.out.println(calculator.calculate(op));
-                 
-            }
-            if (ot == 2) {
-                while (rd.hasNextLine())
-                v.respuesta();
-                System.out.println(calculator.calculate(rd.hasNextLine()));
-        }
-        }
+
         // TODO: Usar un menu e ingresar los contenidos del archivo de texto en el metodo calculate.
     }
 }
--- src/com/calculadora/Vista.java
+++ src/com/calculadora/Vista.java
@@ -3,16 +3,10 @@ package com.calculadora;
 public class Vista {
     public void saludo() {
         System.out.println("Hola y bienvenido a la calculadora");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
     public void menu() {
-        System.out.println("1. Usar calculadora");
-        System.out.println("2. Leer archivo");
-        System.out.println("3. Salir");
+        System.out.println("1. Leer archivo");
+        System.out.println("2. Salir");
     }
     public void ask() {
         System.out.println("Diga la operación que desa realizar");
