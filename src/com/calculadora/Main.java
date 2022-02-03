cee22d9a21d2a8dfbd88457bd6ffbfab1b16fb63
Mark342-prog <84196496+Mark342-prog@users.noreply.github.com>
2022-02-02 18:38:45 -0600

    cambios a main

--- src/com/calculadora/Main.java
+++ src/com/calculadora/Main.java
@@ -1,5 +1,9 @@
 package com.calculadora;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+import javax.naming.ldap.Rdn;
 public class Main {
 
     public static void main(String[] args) {
@@ -7,10 +11,17 @@ public class Main {
         System.out.println("Calculator");
         PostfixCalculator calculator = new PostfixCalculator();
         Scanner sc = new Scanner(System.in);
+        File file = new File("datos.txt");
+        Scanner rd;
+        try {
+            rd = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         Vista v = new Vista();
         v.saludo();
         int ot = 0;
-        while (ot != 2) {
+        while (ot != 3) {
             v.menu();
             ot = sc.nextInt();
             if (ot == 1) {
@@ -20,6 +31,11 @@ public class Main {
                 System.out.println(calculator.calculate(op));
                  
             }
+            if (ot == 2) {
+                while (rd.hasNextLine())
+                v.respuesta();
+                System.out.println(calculator.calculate(rd.hasNextLine()));
+        }
         }
         // TODO: Usar un menu e ingresar los contenidos del archivo de texto en el metodo calculate.
     }
--- src/com/calculadora/Vista.java
+++ src/com/calculadora/Vista.java
@@ -11,7 +11,8 @@ public class Vista {
     }
     public void menu() {
         System.out.println("1. Usar calculadora");
-        System.out.println("2. Salir");
+        System.out.println("2. Leer archivo");
+        System.out.println("3. Salir");
     }
     public void ask() {
         System.out.println("Diga la operación que desa realizar");
